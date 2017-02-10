f297bcdebb02d3c7b856b2560c01772b59543732
Steve Witman <stevewitman@gmail.com>
2017-02-09 23:41:52 -0700

    Add spotify service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { SearchComponent } from './components/search/search.component';
+import { HttpModule } from '@angular/http'
 
 @Component({
   selector: 'app-root',
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [SpotifyService]
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  searchStr: string;
+
+  constructor(private _spotifyService: SpotifyService) {
+    this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
+      console.log(res.artists.items)
+    })
+  }
 
   ngOnInit() {
+  }
+
+  searchMusic() {
+    
   }
 
 }
